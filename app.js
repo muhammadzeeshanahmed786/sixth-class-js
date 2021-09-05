59fb4554235ab8b822d694831322e7481bf01596
Muhammad Zeeshan Ahmed <78692718+shani2103792@users.noreply.github.com>
2021-09-05 11:06:03 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,179 @@
+
+// "<h1>Mark Sheet</h1>"
+
+// var maths=+prompt("Enter your Maths marks")
+// var physics=+prompt("Enter your physcics Marks")
+// var chemistry=+prompt("Enter your chemistry Marks")
+
+// var per=maths+physics+chemistry
+// var div=per/300 *100
+// console.log(div)
+// if(div>=80){
+//     document.write("Your Grade is A-One")
+// }
+// else if(div>=70){
+//     document.write("Your Grade is A")
+// }
+// else if(div>=60){
+//     document.write("Your Grade is B")
+// }
+// else if(div>=50){
+//     document.write("Your Grade is C")
+// }
+
+// else{
+//     document.write("Fail")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var p1=document.getElementById("p")
+
+
+
+// var first=+prompt("Enter First number");
+// var sec=+prompt("Enter second number");
+// var ope=prompt("Enter operator number");
+
+
+
+// if (ope === "+") {
+//     p1.innerHTML=first+sec
+// }
+
+
+
+// if (ope === "-") {
+//     p1.innerHTML=first-sec
+// }
+
+
+// if (ope === "*") {
+//     p1.innerHTML=first*sec
+// }
+
+
+// if (ope === "/") {
+//     p1.innerHTML=first/sec
+// }
+
+
+
+//&& and 
+// || or
+
+
+
+
+
+// var mag="meggi";
+// var pep="pepsi";
+
+
+// if (mag==="meggi" &&  pep==="pepsi") {
+//     console.log(true)
+// }
+
+// else{
+//     console.log(false)
+// }
+
+
+
+
+
+
+// var meg="soup";
+// var pep="dew";
+
+// if(meg === "soup" || pep==="pepsi"){
+//     console.log(true)
+// }
+
+// else{
+//     console.log(false)
+// }
+
+
+
+
+
+// console.log("ddd")
+
+// var pro=+prompt("enter time")
+
+// var p1 =document.getElementById("p")
+
+// if (pro >"0000" && pro <"1200") {
+//     console.log("Good morning")
+// }
+
+
+// if (pro >="1200" && pro <="1700") {
+//     console.log("Good AfterNoon")
+// }
+// if (pro >="1700" && pro <="2100") {
+//     console.log("Good Evening")
+// }
+
+
+// else{
+//     console.log("Good night")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+// var pro1=+prompt("Enter Number");
+// var pro2=+prompt("Enter Number");
+
+// if (pro1>pro2) {
+//     console.log(pro1)
+// }
+
+
+// if (pro1<pro2) {
+//     console.log(pro2)
+// }
+
+
+
+
+// if (pro1===pro2) {
+//     console.log("Equal")
+// }
+
+
+
+// var pro1=+prompt("Enter Number");
+// if (pro1>0) {
+//     alert("Positive")
+// }
+
+
+// if (pro1<0) {
+//     alert("Negative")
+// }
+
+// if (pro1===0) {
+//     alert("zero")
+// }
+
+
